6003dcf34f142d54953173ee5d2b5814f11337b5
Ruben <ruben@one2apps.com>
2017-09-14 10:56:19 +0200

    assessment page'

--- resources/views/assessment/edit.blade.php
+++ resources/views/assessment/edit.blade.php
@@ -7,9 +7,56 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Assessment</div>
 
-                <div class="panel-body">
+                <div class="panel-body" style="padding-top: 0; padding-bottom: 0;">
                     
-                    
+                    <div class="row assessment-users">
+
+                        <div class="col-lg-3">
+
+                            <div class="assessment-users-icon">
+                                <i class="fa fa-users"></i>
+                            </div>
+
+                        </div>
+
+                        <div class="col-lg-3">
+                            <a href="#" class="add-box"><i class="fa fa-plus"></i></a>
+                        </div>
+
+                    </div>
+
+                    <div class="row assessment-devices">
+
+                        <div class="col-lg-3">
+
+                            <div class="assessment-devices-icon">
+                                <i class="fa fa-laptop"></i>
+                            </div>
+
+                        </div>
+
+                        <div class="col-lg-3">
+                            <a href="#" class="add-box"><i class="fa fa-plus"></i></a>
+                        </div>
+
+                    </div>
+
+
+                    <div class="row assessment-policies">
+
+                        <div class="col-lg-3">
+
+                            <div class="assessment-policies-icon">
+                                <i class="fa fa-cogs"></i>
+                            </div>
+
+                        </div>
+
+                        <div class="col-lg-3">
+                            <a href="#" class="add-box"><i class="fa fa-plus"></i></a>
+                        </div>
+
+                    </div>
 
                 </div>
             </div>
